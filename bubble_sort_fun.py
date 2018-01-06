77c9806c122455664b22f2fc126ff7aac0e3a956
YuYang Fong <33592304+yuyangfong@users.noreply.github.com>
2018-01-06 14:02:55 +0800

    Add files via upload

---
+++ bubble_sort_fun.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[66]:
+
+
+# Bubble Sort, Reverse = True則大至小(預設值)；反之小至大
+
+def bubble_sort_fun(x, reverse = 1):
+    for i in range(0, len(x) - 1):
+        for j in range(i + 1, len(x)):
+            if reverse == 0:
+                if x[i] > x[j]:
+                    x[i], x[j] = x[j], x[i]
+            elif reverse == 1:
+                if x[i] < x[j]:
+                    x[i], x[j] = x[j], x[i]
+    return x
+
+
+# In[67]:
+
+
+import random
+lst = random.sample(range(0, 100), 20)
+print('亂數數列:', lst, sep = '\n')
+print('排序數列(大至小):', exchange_sort(lst, reverse = 1), sep = '\n')
+print('排序數列(小至大):', exchange_sort(lst, reverse = 0), sep = '\n')
+
