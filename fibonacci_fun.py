48636929cabeadfd934cfdfb086387c2f269a02d
YuYang Fong <33592304+yuyangfong@users.noreply.github.com>
2018-01-08 14:38:22 +0800

    Update and rename fib_fun.py to fibonacci_fun.py

--- fibonacci_fun.py
+++ fibonacci_fun.py
@@ -1,4 +1,4 @@
-def fib_gen(f1, f2, fib_len):
+def fibonacci_fun(f1, f2, fib_len):
     fibo = [f1, f2]
     while len(fibo) < fib_len:
         f1, f2 = f2 , f1 + f2
@@ -7,5 +7,5 @@ def fib_gen(f1, f2, fib_len):
 
 f1 = int(input("第一個數字:"))
 f2 = int(input("第二個數字:"))
-fib_len = int(input("數列函數:"))
-print(fib_gen(f1, f2, fib_len))
+fibo_len = int(input("數列函數:"))
+print(fibonacci_fun(f1, f2, fibo_len))
